d3a088dfa33b6c8ea567fff8b073fecdb1008250
unknown <shireesha.vani@gmail.com>
2020-08-12 21:43:03 +0530

    Updates

---
+++ precache-manifest.bc812393ccd64d2071ae95f41d81b928.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fc6e84dd6afaea168b834eca3dd455",
+    "url": "/react-assignment-1/index.html"
+  },
+  {
+    "revision": "de9668683d2b4b32fded",
+    "url": "/react-assignment-1/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b76b0568198c5cbd535a",
+    "url": "/react-assignment-1/static/css/main.ee7d8590.chunk.css"
+  },
+  {
+    "revision": "de9668683d2b4b32fded",
+    "url": "/react-assignment-1/static/js/2.13be00f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-assignment-1/static/js/2.13be00f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76b0568198c5cbd535a",
+    "url": "/react-assignment-1/static/js/main.dfe0c17c.chunk.js"
+  },
+  {
+    "revision": "23d521f3be9df2494def",
+    "url": "/react-assignment-1/static/js/runtime-main.04796e42.js"
+  }
+]);
